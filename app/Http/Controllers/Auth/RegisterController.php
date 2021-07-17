ffe096318ad9637f9cc097b75f0fbbd6cf474faf
gaoyanshuo <gaoyanshuo9388@yahoo.com>
2021-07-17 13:13:17 +0900

    captcha

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,11 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'captcha' => ['required', 'captcha'],
+        ],
+        [
+            'captcha.required' => '画像の文字を入力してください',
+            'captcha.captcha' => '正しい文字を入力してください',
         ]);
     }
 
