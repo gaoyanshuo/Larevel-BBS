559ecf82fd238fa3092010d00575e19c2af28d4c
gaoyanshuo <gaoyanshuo9388@yahoo.com>
2021-07-18 22:19:30 +0900

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -2,13 +2,14 @@
 
 namespace App\Handlers;
 use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class ImageUploadHandler
 {
 
     protected $allowed_ext = ["png","jpg","gif","jpeg"];
 
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix,$max_width = false)
     {
 
 
@@ -32,10 +33,30 @@ class ImageUploadHandler
 
         // 将图片移动到我们的目标存储路径中
         $file->move($upload_path, $file_name);
+        if ($max_width && $extension != 'gif'){
+            $this->reduceSize($upload_path . '/' . $file_name, $max_width);
+        }
 
         return [
             'path' =>  "/$folder_name/$file_name"
         ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        // 先实例化，传参是文件的磁盘物理路径
+        $image = Image::make($file_path);
+
+        // 进行大小调整的操作
+        $image->resize($max_width,null,function ($constraint) {
+
+            // 设定宽度是 $max_width，高度等比例缩放
+            $constraint->aspectRatio();
+
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+        $image->save();
     }
 
 }
