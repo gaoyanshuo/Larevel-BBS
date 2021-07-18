36ce0dbf0e7b2cdf1fab957ef5cbdd4a2448656f
gaoyanshuo <gaoyanshuo9388@yahoo.com>
2021-07-19 01:02:56 +0900

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -3,10 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
+use App\Models\User;
+use App\Models\Category;
 class Topic extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function category()
+    {
+        return $this->belongsto(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsto(User::class);
+    }
 }
