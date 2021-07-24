4fbb1900fa2d8c9501d2e6a0c6037137568d0050
gaoyanshuo <gaoyanshuo9388@yahoo.com>
2021-07-24 13:05:55 +0900

    站点设置

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,9 @@
   <!-- CSRF Token -->
   @csrf
 
-  <title>@yield('title', 'LaraBBS') - Laravel BBS</title>
-  <meta name="description" content="@yield('description','LaraBBS')">
+  <title>@yield('title', 'LaraBBS') - {{ setting('site_name','Laravel') }}</title>
+  <meta name="description" content="@yield('description', setting('seo_description','LaraBBS'))">
+  <meta name="keywords" content="@yield('keyword', setting('seo_keyword', 'LaraBBS,community'))" />
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
